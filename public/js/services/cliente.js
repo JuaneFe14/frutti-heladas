bedbd26fc9a009af086b3e46c069aaf3ead1f515
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-12-29 16:30:59 -0500

    revision y arreglos sencillos para mejoras de codigo y funcionamiento

--- public/js/services/cliente.js
+++ public/js/services/cliente.js
@@ -3,39 +3,39 @@ const ref = db.collection('cliente') // referencia a la coleccion (tabla)
 const clientes = [] // array para almacenar los datos de la coleccion
 
 // atributos del formulario para el DOM
-const documento = document.getElementById('documento')
-const nombre = document.getElementById('nombre')
-const apellido = document.getElementById('apellido')
-const correo = document.getElementById('correo')
-const fechaNacimiento = document.getElementById('fechaNacimiento')
+const documento = document.getElementById('documento');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const correo = document.getElementById('correo');
+const fechaNacimiento = document.getElementById('fechaNacimiento');
 
 // botones de accion del formulario
-const btnFactura = document.getElementById('btnFactura')
-const btnCrearCliente = document.getElementById('btnCrearCliente')
+const btnFactura = document.getElementById('btnFactura');
+const btnCrearCliente = document.getElementById('btnCrearCliente');
 
-    // #endregion
+// #endregion
 
-    //#region funciones
+//#region funciones
 
-    // Obtener los datos completos de una coleccion (tabla)
-    (() => {
-        ref.get().then((querySnapshot) => {
-            if (querySnapshot.empty) {
-                console.log('No hay datos')
-            }
-            for (let i = 0; i < querySnapshot.docs.length; i++) {
-                clientes.push(querySnapshot.docs[i].data())
-            }
-        })
-        btnFactura.classList.add('disabled')
-    })()
+// Obtener los datos completos de una coleccion (tabla)
+(() => {
+    ref.get().then((querySnapshot) => {
+        if (querySnapshot.empty) {
+            console.log('No hay datos');
+        }
+        for (let i = 0; i < querySnapshot.docs.length; i++) {
+            clientes.push(querySnapshot.docs[i].data());
+        }
+    });
+    btnFactura.classList.add('disabled');
+})();
 
 // buscar un cliente por su documento
-const clienteByDocument = (documento) => clientes.find((cliente) => cliente.documento === documento)
+const clienteByDocument = (documento) => clientes.find((cliente) => cliente.documento === documento);
 
 // obtener un unico dato de la coleccion previa con un metodo de un array
 function getById() {
-    const searchCliente = clienteByDocument(documento.value.trim())
+    const searchCliente = clienteByDocument(documento.value.trim());
     if (searchCliente) {
         //mapear los datos en el formulario
         documento.value = searchCliente.documento
@@ -43,21 +43,20 @@ function getById() {
         apellido.value = searchCliente.apellido
         correo.value = searchCliente.correo
         fechaNacimiento.value = searchCliente.fechaNacimiento
-        console.log(searchCliente.id);
 
-        btnFactura.classList.remove('disabled')
-        btnFactura.focus()
-        location.href = '/factura.html'
+        btnFactura.classList.remove('disabled');
+        btnFactura.focus();
+        // location.href = '/factura.html'
     } else {
-        nombre.focus()
+        nombre.focus();
     }
 }
 
 // crear un nuevo documento en la coleccion
 function create() {
     if (documento.value.trim() === '' || nombre.value.trim() === '' || apellido.value.trim === '' || correo.value.trim() === '' || fechaNacimiento.value.trim() === '') {
-        documento.focus()
-        return
+        documento.focus();
+        return;
     }
 
     // Add a second document with a generated ID.
@@ -69,20 +68,20 @@ function create() {
         fechaNacimiento: `${fechaNacimiento.value.trim()}`
     })
         .then((docRef) => {
-            console.log('Document written with ID: ', docRef.id)
-            location.href = '/factura.html'
+            console.log('Document written with ID: ', docRef.id);
+            location.href = '/factura.html';
         })
         .catch((error) => {
-            console.error('Error adding document: ', error)
-            documento.focus()
-        })
+            console.error('Error adding document: ', error);
+            documento.focus();
+        });
 }
 
 // actualizar un documento en la coleccion
 function update() {
     if (documento.value.trim() === '' || nombre.value.trim() === '' || apellido.value.trim() === '' || correo.value.trim() === '' || fechaNacimiento.value.trim() === '') {
-        documento.focus()
-        return
+        documento.focus();
+        return;
     }
 
     ref.doc(`${searchCliente.id}`).update({
@@ -93,22 +92,23 @@ function update() {
         fechaNacimiento: `${fechaNacimiento.value.trim()}`
     })
         .then(() => {
-            console.log('Document successfully updated!')
+            console.log('Document successfully updated!');
+            btnFactura.focus();
         })
         .catch((error) => {
-            console.error('Error updating document: ', error)
-            documento.focus()
+            console.error('Error updating document: ', error);
+            documento.focus();
         })
 }
 //#endregion
 
 //#region eventos
 btnCrearCliente.addEventListener('click', () => {
-    const searchCliente = clienteByDocument(documento.value.trim())
+    const searchCliente = clienteByDocument(documento.value.trim());
     if (searchCliente) {
-        update()
+        update();
     } else {
-        create()
+        create();
     }
 })
 //#endregion
