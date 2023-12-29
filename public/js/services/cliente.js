40d7d5365c713ce9f4a542c7d7c6e0e0c24e01fe
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-12-29 15:28:50 -0500

    arreglo de navegacion y agregado eslint

--- public/js/services/cliente.js
+++ public/js/services/cliente.js
@@ -1,50 +1,45 @@
-//#region atributos
-const ref = db.collection("cliente");
-const clientes = [];
+// #region atributos
+const ref = db.collection('cliente')
+const clientes = []
 
-const documento = document.getElementById("documento");
-const nombre = document.getElementById("nombre");
-const apellido = document.getElementById("apellido");
-const correo = document.getElementById("correo");
-const fechaNacimiento = document.getElementById("fechaNacimiento");
+const documento = document.getElementById('documento')
+const nombre = document.getElementById('nombre')
+const apellido = document.getElementById('apellido')
+const correo = document.getElementById('correo')
+const fechaNacimiento = document.getElementById('fechaNacimiento')
 
-const btnFactura = document.getElementById("btnFactura");
-const btnCrearCliente = document.getElementById("btnCrearCliente");
+const btnFactura = document.getElementById('btnFactura')
+const btnCrearCliente = document.getElementById('btnCrearCliente');
 
-//#endregion
-
+// #endregion
 
 (() => {
     ref.get().then((querySnapshot) => {
         if (querySnapshot.empty) {
-            console.log("No hay datos");
+            console.log('No hay datos')
         }
         for (let i = 0; i < querySnapshot.docs.length; i++) {
             clientes.push(querySnapshot.docs[i].data())
         }
-    });
-    btnFactura.classList.add("disabled");
+    })
+    btnFactura.classList.add('disabled')
 })()
 
 function getById() {
     const searchCliente = clientes.find((cliente) => cliente.documento === documento.value.trim())
     if (searchCliente) {
-        alert("El cliente ya existe");
-        btnFactura.classList.remove("disabled");
-        location.href = "/public/factura.html"
-        return;
+        alert('El cliente ya existe')
+        btnFactura.classList.remove('disabled')
+        btnFactura.focus()
     } else {
-        nombre.focus();
+        nombre.focus()
     }
 }
 
-
-
 function create() {
-
-    if (documento.value.trim() === "" || nombre.value.trim() === "" || apellido.value().trim === "" || correo.value.trim() === "" || fechaNacimiento.value.trim() === "") {
-        documento.focus();
-        return;
+    if (documento.value.trim() === '' || nombre.value.trim() === '' || apellido.value().trim === '' || correo.value.trim() === '' || fechaNacimiento.value.trim() === '') {
+        documento.focus()
+        return
     }
 
     // Add a second document with a generated ID.
@@ -56,20 +51,20 @@ function create() {
         fechaNacimiento: `${fechaNacimiento.value.trim()}`
     })
         .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
+            console.log('Document written with ID: ', docRef.id)
         })
         .catch((error) => {
-            console.error("Error adding document: ", error);
-            documento.focus();
-        });
+            console.error('Error adding document: ', error)
+            documento.focus()
+        })
 }
 
 function update() {
     const searchCliente = clientes.find((cliente) => cliente.documento === documento.value.trim())
     if (searchCliente) {
-        if (documento.value.trim() === "" || nombre.value.trim() === "" || apellido.value.trim() === "" || correo.value.trim() === "" || fechaNacimiento.value.trim() === "") {
-            documento.focus();
-            return;
+        if (documento.value.trim() === '' || nombre.value.trim() === '' || apellido.value.trim() === '' || correo.value.trim() === '' || fechaNacimiento.value.trim() === '') {
+            documento.focus()
+            return
         }
 
         ref.doc(`${id}`).update({
@@ -80,11 +75,11 @@ function update() {
             fechaNacimiento: `${fechaNacimiento.value.trim()}`
         })
             .then(() => {
-                console.log("Document successfully updated!");
+                console.log('Document successfully updated!')
             })
             .catch((error) => {
-                console.error("Error updating document: ", error);
-                documento.focus();
-            });
+                console.error('Error updating document: ', error)
+                documento.focus()
+            })
     }
 }
